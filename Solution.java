265ce3ad2efc1f1271e1f05bc5c9443698eefe54
Tarun-2001 <65804743+Tarun-2001@users.noreply.github.com>
2022-01-25 12:18:26 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean detectCapitalUse(String w) {
-        if(w.equals(w.toUpperCase())) return true;
+        if(w.equals(w.toUpperCase())) return true;//returning boolean value true
         else if(w.equals(w.toLowerCase())) return true;
-        else if(w.substring(0,1)>=65&&w.substring(0,1)<=90&&(w.substring(1).equals(w.substring(1).toLowerCase()))) 
+        else if(w.substring(0,1)>=65&&w.substring(0,1)<=90&&(w.substring(1).equals(w.substring(1).toLowerCase()))) return true;
+        return false;
     }
 }
