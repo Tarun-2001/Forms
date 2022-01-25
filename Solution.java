6e50c5614c345bae76c38694467989edece50dbf
Tarun-2001 <65804743+Tarun-2001@users.noreply.github.com>
2022-01-25 12:13:43 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,6 +3,5 @@ class Solution {
         if(w.equals(w.toUpperCase())) return true;
         else if(w.equals(w.toLowerCase())) return true;
         else if(w.substring(0,1)>=65&&w.substring(0,1)<=90&&(w.substring(1).equals(w.substring(1).toLowerCase()))) 
-        return false;
     }
 }
